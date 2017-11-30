b250b5014783979073db73bb8e09c25109010c9b
Andrew <drsixthsense@mail.ru>
2017-11-30 18:41:36 +0100

    HW4 main complete

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.model;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
